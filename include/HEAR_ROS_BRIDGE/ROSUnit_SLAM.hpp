d4cb23f4ed857e17ef176aa4208ea00c7baa7a51
AhmedHumais <m.ahmedhumais@gmail.com>
2020-12-07 17:31:17 +0400

    ROSUnitSLAM v1.0.1 static tf for opti_frame added and yaw added

--- include/HEAR_ROS_BRIDGE/ROSUnit_SLAM.hpp
+++ include/HEAR_ROS_BRIDGE/ROSUnit_SLAM.hpp
@@ -2,27 +2,43 @@
 #include "ros/ros.h"
 #include <nav_msgs/Odometry.h>
 #include <tf2_ros/transform_listener.h>
+#include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/Vector3Stamped.h>
-#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 #include "HEAR_ROS_BRIDGE/ROSUnit.hpp"
+#include "HEAR_msg/BoolMsg.hpp"
 #include "HEAR_msg/Vector3DMsg.hpp"
 #include "HEAR_core/Timer.hpp"
+
+#include <string>
 
 class ROSUnit_SLAM : public ROSUnit {
 private:
     static ROSUnit_SLAM* _instance_ptr; 
     static tf2_ros::Buffer tf_Buffer;
     static tf2_ros::TransformListener tfListener;
+    static tf2_ros::StaticTransformBroadcaster static_broadcaster;
     ros::Subscriber _sub_odometry;
     static Port* _output_port_0;
     static Port* _output_port_1;
+    static Port* _output_port_2;
     static void callbackOdom(const nav_msgs::Odometry::ConstPtr&);
-
+    static std::string ref_frame;
+    void pub_static_transform();
+//    geometry_msgs::TransformStamped static_transformStamped;
+    Vector3D<float> _position;
+    Vector3D<float> _orientation;
+    Port* _input_port_0;
+    Port* _input_port_1;
+    Port* _input_port_2;
+    
 public:
     ROSUnit_SLAM(ros::NodeHandle&);
-    enum ports_id {OP_0_POS, OP_1_VEL};
-    void process(DataMsg* t_msg, Port* t_port) {};
+    enum ports_id {OP_0_POS, OP_1_ORI, OP_2_VEL, IP_0_POS, IP_1_ORI, IP_2_TRIGGER};
+    void process(DataMsg* t_msg, Port* t_port);
     ~ROSUnit_SLAM();
 };
--- src/ROSUnit_SLAM.cpp
+++ src/ROSUnit_SLAM.cpp
@@ -4,40 +4,55 @@ ROSUnit_SLAM* ROSUnit_SLAM::_instance_ptr = NULL;
 tf2_ros::Buffer ROSUnit_SLAM::tf_Buffer;
 Port* ROSUnit_SLAM::_output_port_0;
 Port* ROSUnit_SLAM::_output_port_1;
+Port* ROSUnit_SLAM::_output_port_2;
 tf2_ros::TransformListener ROSUnit_SLAM::tfListener(ROSUnit_SLAM::tf_Buffer);
+tf2_ros::StaticTransformBroadcaster ROSUnit_SLAM::static_broadcaster;
+std::string ROSUnit_SLAM::ref_frame = "map";
 
 ROSUnit_SLAM::ROSUnit_SLAM(ros::NodeHandle& t_main_handler): ROSUnit(t_main_handler){
     _sub_odometry = t_main_handler.subscribe<nav_msgs::Odometry>("rtabmap/pose", 2, callbackOdom, ros::TransportHints().tcpNoDelay());
     _instance_ptr = this;
     _output_port_0 = new OutputPort(ports_id::OP_0_POS, this);
-    _output_port_1 = new OutputPort(ports_id::OP_1_VEL, this);
-    _ports = {_output_port_0, _output_port_1};
+    _output_port_1 = new OutputPort(ports_id::OP_1_ORI, this);
+    _output_port_2 = new OutputPort(ports_id::OP_2_VEL, this);
+    _input_port_0 = new InputPort(ports_id::IP_0_POS, this);
+    _input_port_1 = new InputPort(ports_id::IP_1_ORI, this);
+    _input_port_2 = new InputPort(ports_id::IP_2_TRIGGER, this);
+    _ports = {_output_port_0, _output_port_1, _output_port_1, _input_port_0,_input_port_1, _input_port_2};
 }
 
 void ROSUnit_SLAM::callbackOdom(const nav_msgs::Odometry::ConstPtr& od_msg){
-    Vector3DMsg pos_msg, vel_msg;
-    Vector3D<float> pos_data, vel_data;
-    geometry_msgs::PointStamped pos, tf_pos;
+    Vector3DMsg pos_msg, vel_msg, ori_msg;
+    Vector3D<float> pos_data, vel_data, ori_data;
+    geometry_msgs::PoseStamped pos, tf_pos;
     geometry_msgs::Vector3Stamped vel, tf_vel;
     pos.header = od_msg->header;
     vel.header = od_msg->header;
-    pos.point = (*od_msg).pose.pose.position;
+    pos.pose = (*od_msg).pose.pose;
     vel.vector = (*od_msg).twist.twist.linear;
     try{
-        tf_Buffer.transform(pos, tf_pos, "map", ros::Duration(0.05));
+        tf_Buffer.transform(pos, tf_pos, ref_frame, ros::Duration(0.05));
     }
     catch (tf2::TransformException &ex) {
         ROS_WARN("Failure %s\n", ex.what()); //Print exception which was caught
     }
     try{
-        tf_Buffer.transform(vel, tf_vel, "map", ros::Duration(0.05));
+        tf_Buffer.transform(vel, tf_vel, ref_frame, ros::Duration(0.05));
     }
     catch (tf2::TransformException &ex) {
         ROS_WARN("Failure %s\n", ex.what()); //Print exception which was caught
     }
-    pos_data.x = tf_pos.point.x;
-    pos_data.y = tf_pos.point.y;
-    pos_data.z = tf_pos.point.z;
+    pos_data.x = tf_pos.pose.position.x;
+    pos_data.y = tf_pos.pose.position.y;
+    pos_data.z = tf_pos.pose.position.z;
+
+    tf2::Quaternion quat(tf_pos.pose.orientation.x, tf_pos.pose.orientation.y, tf_pos.pose.orientation.z, tf_pos.pose.orientation.w);
+    tf2::Matrix3x3 mat(quat);
+    tf2Scalar yaw, pitch, roll;
+    mat.getRPY(roll, pitch, yaw);
+    ori_data.x = (float)roll;
+    ori_data.y = (float)pitch;
+    ori_data.z = (float)yaw;
 
     vel_data.x = tf_vel.vector.x;
     vel_data.y = tf_vel.vector.y;
@@ -45,8 +60,41 @@ void ROSUnit_SLAM::callbackOdom(const nav_msgs::Odometry::ConstPtr& od_msg){
 
     pos_msg.data = pos_data;
     vel_msg.data = vel_data;
+    ori_msg.data = ori_data;
 
     _instance_ptr->_output_port_0->receiveMsgData(&pos_msg);
-    _instance_ptr->_output_port_1->receiveMsgData(&vel_msg);
+    _instance_ptr->_output_port_1->receiveMsgData(&ori_msg);
+    _instance_ptr->_output_port_2->receiveMsgData(&vel_msg);
+}
 
+void ROSUnit_SLAM::process(DataMsg* t_msg, Port* t_port){
+    if(t_port->getID() == ports_id::IP_0_POS){
+        this->_position = ((Vector3DMsg*)t_msg)->data;
+    }
+    else if(t_port->getID() == ports_id::IP_1_ORI){
+        this->_orientation = ((Vector3DMsg*)t_msg)->data;
+    }
+    else if(t_port->getID() == ports_id::IP_2_TRIGGER){
+        if (((BoolMsg*)t_msg)->data){
+            this->pub_static_transform();
+            ref_frame = "opti_frame";
+        }
+    }
+}
+
+void ROSUnit_SLAM::pub_static_transform(){
+    geometry_msgs::TransformStamped static_transformStamped;
+    static_transformStamped.header.stamp = ros::Time::now();
+    static_transformStamped.header.frame_id = "opti_frame";
+    static_transformStamped.child_frame_id = "map";
+    static_transformStamped.transform.translation.x = this->_position.x;
+    static_transformStamped.transform.translation.y = this->_position.y;
+    static_transformStamped.transform.translation.z = this->_position.z;
+    tf2::Quaternion quat;
+    quat.setRPY(this->_orientation.x, this->_orientation.y, this->_orientation.z);
+    static_transformStamped.transform.rotation.x = quat.x();
+    static_transformStamped.transform.rotation.y = quat.y();
+    static_transformStamped.transform.rotation.z = quat.z();
+    static_transformStamped.transform.rotation.w = quat.w();
+    static_broadcaster.sendTransform(static_transformStamped);
 }
