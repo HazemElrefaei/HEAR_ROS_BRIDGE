d6848001e7ba43b39aaab479bc57fae252b67a2d
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-01-17 14:50:19 +0400

    .

--- src/ROSUnit_IMU.cpp
+++ src/ROSUnit_IMU.cpp
@@ -10,18 +10,20 @@ Port* ROSUnit_IMU::_output_port_1;
 Port* ROSUnit_IMU::_output_port_2;
 Port* ROSUnit_IMU::_output_port_3;
 Port* ROSUnit_IMU::_output_port_4;
+Port* ROSUnit_IMU::_output_port_5;
 
 ROSUnit_IMU::ROSUnit_IMU(ros::NodeHandle& t_main_handler) : ROSUnit(t_main_handler) {
     _sub_attitude = t_main_handler.subscribe("filter/quaternion", 2, callbackXsensAttitude, ros::TransportHints().tcpNoDelay());
     _sub_body_rate = t_main_handler.subscribe("imu/angular_velocity", 2, callbackXsensBodyRate, ros::TransportHints().tcpNoDelay());
-    _sub_acceleration = t_main_handler.subscribe("filter/free_acceleration", 2, callbackXsensFreeAcceleration, ros::TransportHints().tcpNoDelay());
+    _sub_accelerometer = t_main_handler.subscribe("imu/acceleration", 2, callbackXsensFreeAcceleration, ros::TransportHints().tcpNoDelay());
     _instance_ptr = this;
     _output_port_0 = new OutputPort(ports_id::OP_0_ROLL, this);
     _output_port_1 = new OutputPort(ports_id::OP_1_PITCH, this);
     _output_port_2 = new OutputPort(ports_id::OP_2_ROLL_RATE, this);
     _output_port_3 = new OutputPort(ports_id::OP_3_PITCH_RATE, this);
     _output_port_4 = new OutputPort(ports_id::OP_4_YAW_RATE, this);
-    _ports = {_output_port_0, _output_port_1, _output_port_2, _output_port_3, _output_port_4};
+    _output_port_5 = new OutputPort(ports_id::OP_5_FREE_ACCELERATION, this);
+    _ports = {_output_port_0, _output_port_1, _output_port_2, _output_port_3, _output_port_4, _output_port_5};
 }
 
 ROSUnit_IMU::~ROSUnit_IMU() {
@@ -31,8 +33,8 @@ ROSUnit_IMU::~ROSUnit_IMU() {
 void ROSUnit_IMU::callbackXsensBodyRate(const geometry_msgs::Vector3Stamped& msg_bodyrate){
     Vector3DMsg pv_dot_msg;
     Vector3D<double> angular_vel;
-    angular_vel.x = msg_bodyrate.vector.x;
-    angular_vel.y = msg_bodyrate.vector.y;
+    angular_vel.x = -1 * msg_bodyrate.vector.x;
+    angular_vel.y = -1 * msg_bodyrate.vector.y;
     angular_vel.z = msg_bodyrate.vector.z;
     pv_dot_msg.data = angular_vel;
     FloatMsg roll_rate, pitch_rate, yaw_rate;
@@ -48,10 +50,11 @@ void ROSUnit_IMU::callbackXsensBodyRate(const geometry_msgs::Vector3Stamped& msg
 void ROSUnit_IMU::callbackXsensFreeAcceleration(const geometry_msgs::Vector3Stamped& msg_free_acceleration){
     Vector3DMsg pv_dot_dot_msg;
     Vector3D<double> free_acceleration;
-    free_acceleration.x = msg_free_acceleration.vector.x;
-    free_acceleration.y = msg_free_acceleration.vector.y;
+    free_acceleration.x = -1 * msg_free_acceleration.vector.x;
+    free_acceleration.y = -1 * msg_free_acceleration.vector.y;
     free_acceleration.z = msg_free_acceleration.vector.z;
     pv_dot_dot_msg.data = free_acceleration;
+    _instance_ptr->_output_port_5->receiveMsgData(&pv_dot_dot_msg);
 }
 
 void ROSUnit_IMU::callbackXsensAttitude( const geometry_msgs::QuaternionStamped& msg_attitude){
@@ -94,11 +97,11 @@ void ROSUnit_IMU::callbackXsensAttitude( const geometry_msgs::QuaternionStamped&
     _instance_ptr->_output_port_1->receiveMsgData(&pitch);
 }
 
-void ROSUnit_IMU::callbackXsensVelocity(const geometry_msgs::TwistStamped& msg_velocity){
-    Vector3D<double> velocity;
-	Vector3DMsg velocity_msg;
-    velocity.x = msg_velocity.twist.linear.x;
-    velocity.y = msg_velocity.twist.linear.y;
-    velocity.z = msg_velocity.twist.linear.z;
-	velocity_msg.data = velocity;
-}
+// void ROSUnit_IMU::callbackXsensVelocity(const geometry_msgs::TwistStamped& msg_velocity){
+//     Vector3D<double> velocity;
+// 	Vector3DMsg velocity_msg;
+//     velocity.x = msg_velocity.twist.linear.x;
+//     velocity.y = msg_velocity.twist.linear.y;
+//     velocity.z = msg_velocity.twist.linear.z;
+// 	velocity_msg.data = velocity;
+// }
